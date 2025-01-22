15fcad060bbc7c66e8ef0eccf0e06c4bd7490b1a
Phillip Sabatino <pdsabatino@gmail.com>
2025-01-22 03:28:41 -0500

    Update for production proxy url changes and update icon

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: '/api', // Matches the Vite proxy prefix
+  baseURL: import.meta.env.VITE_API_URL, // Matches the Vite proxy prefix
   timeout: 5000, // Optional: Request timeout
 })
 
